9172ac50840d74bed8181675a34b51a49f6d286f
johnnyunar <johnunar@gmail.com>
2024-12-25 23:25:54 +0100

    Improve basic service detail

--- registry/views.py
+++ registry/views.py
@@ -9,3 +9,8 @@ class ServiceDetail(DetailView):
     context_object_name = "service"
     slug_field = "uuid"
     slug_url_kwarg = "uuid"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["rating_range"] = range(1, 6)
+        return context
