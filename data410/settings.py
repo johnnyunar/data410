ccd9a1cc186735186508d3ff6daf8c57041b2d80
johnnyunar <johnunar@gmail.com>
2025-01-07 19:44:11 +0100

    Remove ssl_cert_reqs redis setting

--- data410/settings.py
+++ data410/settings.py
@@ -126,11 +126,6 @@ CACHES = {
     }
 }
 
-if not DEBUG:
-    CACHES["default"]["OPTIONS"]["CONNECTION_POOL_KWARGS"] = {
-        "ssl_cert_reqs": None,
-    }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
